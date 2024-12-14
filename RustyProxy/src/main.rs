045da4bed5d5106bd3067a938abb1a0c908cb3f2
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-12-14 00:15:27 -0300

    Update main.rs

--- RustyProxy/src/main.rs
+++ RustyProxy/src/main.rs
@@ -33,23 +33,8 @@ fn start_http(listener: TcpListener) {
 }
 
 fn handle_client(client_stream: &mut TcpStream) -> Result<(), Error> {
-    let status = get_status();
-    client_stream.write_all(format!("HTTP/1.1 101 {}\r\n\r\n", status).as_bytes())?;
-
     client_stream.set_read_timeout(Some(Duration::from_secs(30)))?;
     client_stream.set_write_timeout(Some(Duration::from_secs(30)))?;
-
-    match peek_stream(client_stream) {
-        Ok(data_str) => {
-            if is_websocket(&data_str) {
-                client_stream.write_all(
-                    b"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n",
-                )?;
-                return Ok(());
-            }
-        }
-        Err(e) => return Err(e),
-    }
 
     let addr_proxy = determine_proxy(client_stream)?;
 
@@ -81,10 +66,6 @@ fn transfer_data(read_stream: &mut TcpStream, write_stream: &mut TcpStream) {
                 break;
             }
             Ok(n) => {
-                if n > MAX_BUFFER_SIZE {
-                    eprintln!("Requisição excede o tamanho máximo permitido.");
-                    break;
-                }
                 if let Err(e) = write_stream.write_all(&buffer[..n]) {
                     eprintln!("Erro de escrita: {}. Encerrando conexão.", e);
                     break;
@@ -97,16 +78,8 @@ fn transfer_data(read_stream: &mut TcpStream, write_stream: &mut TcpStream) {
         }
     }
 
-    read_stream.shutdown(Shutdown::Read).ok();
-    write_stream.shutdown(Shutdown::Write).ok();
-}
-
-fn peek_stream(read_stream: &TcpStream) -> Result<String, Error> {
-    let mut peek_buffer = vec![0; 2048];
-    let bytes_peeked = read_stream.peek(&mut peek_buffer)?;
-    let data = &peek_buffer[..bytes_peeked];
-    let data_str = String::from_utf8_lossy(data);
-    Ok(data_str.to_string())
+    let _ = read_stream.shutdown(Shutdown::Read);
+    let _ = write_stream.shutdown(Shutdown::Write);
 }
 
 fn determine_proxy(client_stream: &mut TcpStream) -> Result<String, Error> {
@@ -144,6 +117,13 @@ fn is_openvpn(data: &str) -> bool {
     data.contains("OpenVPN") || data.contains("\x38\x10\x02\x00")
 }
 
+fn peek_stream(read_stream: &TcpStream) -> Result<String, Error> {
+    let mut peek_buffer = vec![0; 2048];
+    let bytes_peeked = read_stream.peek(&mut peek_buffer)?;
+    let data = &peek_buffer[..bytes_peeked];
+    Ok(String::from_utf8_lossy(data).to_string())
+}
+
 fn attempt_connection_with_backoff(addr_proxy: &str) -> Result<TcpStream, Error> {
     let mut retries = 0;
     let max_retries = 5;
@@ -154,9 +134,10 @@ fn attempt_connection_with_backoff(addr_proxy: &str) -> Result<TcpStream, Error>
             Ok(stream) => return Ok(stream),
             Err(e) if retries < max_retries => {
                 eprintln!(
-                    "Erro ao conectar ao proxy {}. Tentando novamente em {} segundos...",
+                    "Erro ao conectar ao proxy {}. Tentando novamente em {} segundos... (Erro: {})",
                     addr_proxy,
-                    delay.as_secs()
+                    delay.as_secs(),
+                    e
                 );
                 thread::sleep(delay);
                 retries += 1;
@@ -174,39 +155,17 @@ fn attempt_connection_with_backoff(addr_proxy: &str) -> Result<TcpStream, Error>
 }
 
 fn get_port() -> u16 {
-    let args: Vec<String> = env::args().collect();
-    let mut port = 80;
-    for i in 1..args.len() {
-        if args[i] == "--port" {
-            if i + 1 < args.len() {
-                port = args[i + 1].parse().unwrap_or(80);
-            }
-        }
-    }
-    port
-}
-
-fn get_status() -> String {
-    let args: Vec<String> = env::args().collect();
-    let mut status = String::from("@RustyManager");
-    for i in 1..args.len() {
-        if args[i] == "--status" {
-            if i + 1 < args.len() {
-                status = args[i + 1].clone();
-            }
-        }
-    }
-    status
+    env::args().nth(1).and_then(|arg| arg.parse().ok()).unwrap_or(80)
 }
 
 fn get_ssh_address() -> String {
-    env::var("SSH_PROXY_ADDR").unwrap_or_else(|_| String::from("0.0.0.0:22"))
+    env::var("SSH_PROXY_ADDR").unwrap_or_else(|_| "127.0.0.1:22".to_string())
 }
 
 fn get_openvpn_address() -> String {
-    env::var("OPENVPN_PROXY_ADDR").unwrap_or_else(|_| String::from("0.0.0.0:1194"))
+    env::var("OPENVPN_PROXY_ADDR").unwrap_or_else(|_| "127.0.0.1:1194".to_string())
 }
 
 fn get_http_proxy_address() -> String {
-    env::var("HTTP_PROXY_ADDR").unwrap_or_else(|_| String::from("0.0.0.0:8080"))
+    env::var("HTTP_PROXY_ADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string())
 }
